5182c95633a57b8c9f3290372c3dd64f3e8d9a20
_Vulpes <vulpesk1tzune@gmail.com>
2026-01-04 12:13:58 +0100

    completed tests

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -1,7 +1,7 @@
 // Tests are important to ensure that your code does what you think it should
 // do.
 
-fn is_even(n: i64) -> bool {
+pub fn is_even(n: i64) -> bool {
     n % 2 == 0
 }
 
@@ -13,11 +13,12 @@ fn main() {
 mod tests {
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
+    use super::is_even;
 
     #[test]
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+        assert!(is_even(0));
+        assert!(is_even(2));
     }
 }
